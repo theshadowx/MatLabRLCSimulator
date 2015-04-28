6e9c99f758342213e6c0f943f3a764b0ee62517a
Ali Diouri <alidiouri@gmail.com>
2015-04-28 11:06:51 +0100

    Remove blank lines

--- RLCEqu.m
+++ RLCEqu.m
@@ -2,9 +2,6 @@
 function f=RLCEqu(i,j)
 global C L R b Signal E;
 
-
-
-
 % Input signal
 if Signal==1
 	b=(E*pulstran(i,0:1:20,'rectpuls',2));										% Step 
@@ -14,7 +11,7 @@ elseif Signal==3
 	b=(E*sawtooth(i,0.5));														% triangle wave
 end
 
-
+% Differential Equation
 f=zeros(2,1);
 f(1)=j(2);
 f(2)=(-1/(L*C))*j(1)+(-R/L)*j(2)+b/(L*C);
